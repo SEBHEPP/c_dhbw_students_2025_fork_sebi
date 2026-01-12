de885109b38805420db7c1f9269ffe900ac46b84
HEPPSEB <inf25152@lehre.dhbw-stuttgart.de>
2026-01-12 16:00:06 +0100

    0010_Exercise

--- 0010_DYNAMIC_MEMORY_MANAGEMENT/Exercise_dynamic_array/main.c
+++ 0010_DYNAMIC_MEMORY_MANAGEMENT/Exercise_dynamic_array/main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sum(int* arr, long size) {
+    long total = 0;
+    for (long i = 0; i < size; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+
 int main() 
 {
+    long size;
+    printf("Enter the number of elements for the integer array: ");
+    scanf("%ld", &size);
 
+    if (size < 0) {
+        printf("\nSize cannot be negative.\n");
+        return 1;        
+    }
+
+    int* array = (int*) malloc(size * sizeof(int));
+
+    if (array == NULL) {
+        printf("\nMemory allocation failed.\n");
+        return 1;
+    }
+
+    printf("\nEnter %ld numbers: ", size);
+    for (long i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    printf("\nThe sum of the array elements is: %d", sum(array, size));
+
+
+    free(array);
+    array = NULL;
 
     return 0;
 }
--- 0010_DYNAMIC_MEMORY_MANAGEMENT/intro/main.c
+++ 0010_DYNAMIC_MEMORY_MANAGEMENT/intro/main.c
@@ -10,5 +10,18 @@ int main()
     printf("\nsizeof(int): %llu", sizeof(int));
     printf("\nmemoryReq: %lld", memoryReq);
 
+    int* heapArray = (int*) malloc(memoryReq);
+
+    for (size_t i = 0; i < length; i++) {
+        heapArray[i] = (int)i*100;
+    }
+    for (size_t i = 0; i < length; i++) {
+        printf("\nheapArray[%lld]: %d", i, heapArray[i]);
+        printf("\n Address of heapArray at %lld: %p", i, (void*)&heapArray[i]);
+    }
+
+    free(heapArray);
+    heapArray = NULL;
+
     return 0;
 }
