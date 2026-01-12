1a57eecec966ecf08cf33aafaca6834501d4d6fc
HEPPSEB <inf25152@lehre.dhbw-stuttgart.de>
2026-01-12 16:08:00 +0100

    Rewrote 0010_intro

--- 0010_DYNAMIC_MEMORY_MANAGEMENT/intro/main.c
+++ 0010_DYNAMIC_MEMORY_MANAGEMENT/intro/main.c
@@ -3,6 +3,29 @@
 
 int main()
 {
-
+    long size;
+    printf("Enter the number of elements for the integer array: ");
+    scanf("%ld", &size);
+    if (size < 0) {
+        printf("\nSize cannot be negative.\n");
+        return 1;
+    }
+    int* array = (int*) malloc(size * sizeof(int));
+    if (array == NULL) {
+        printf("\nMemory allocation failed.\n");
+        return 1;
+    }
+    printf("\nEnter %ld numbers: ", size);
+    for (long i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+    }
+    int total = 0;
+    for (long i = 0; i < size; i++) {
+        total += array[i];
+    }
+    printf("\nThe sum of the array elements is: %d", total);
+    free(array);
+    array = NULL;
+    
     return 0;
 }
