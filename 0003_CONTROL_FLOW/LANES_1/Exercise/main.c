bf1cb6411f92fd3cfe53fc8a4ab5b605f9e3e4ce
HEPPSEB <inf25152@lehre.dhbw-stuttgart.de>
2026-01-27 01:15:51 +0100

    0003

--- 0003_CONTROL_FLOW/LANES_1/Exercise/main.c
+++ 0003_CONTROL_FLOW/LANES_1/Exercise/main.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
 
-int main()
+enum LaneAssociationType {
+    LEFT_LANE,
+    CENTER_LANE,
+    RIGHT_LANE
+};
+
+
+int main(void)
 {
     printf("Create the properties of a vehicle.\n");
 
+    int speed;
+    printf("Speed in m/s: ");
+    scanf("%d", &speed);
+
+    int lane;
+    printf("Lane (1=Left, 2=Center, 3=Right): ");
+    scanf("%d", &lane);
+    lane--;
+
+    printf("               L           C           R               \n");
+    printf("         |           |           |           |         \n");
+    
+    printf("         |     ");
+    if (lane == LEFT_LANE) {
+        printf("X");
+    } else {
+        printf(" ");
+    }
+    printf("     |     ");
+    if (lane == CENTER_LANE) {
+        printf("X");
+    } else {
+        printf(" ");
+    }
+    printf("     |     ");
+    if (lane == RIGHT_LANE) {
+        printf("X");
+    } else {
+        printf(" ");
+    }
+    printf("     |         \n");
+
+
+    printf("         |           |           |           |         \n");
+
+    
 
     return 0;
 }
